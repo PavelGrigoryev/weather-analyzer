e1998572fadc0a63d77718bf7a2a9e135fc9f820
Grigoryev Pavel <Green-ono@yandex.ru>
2023-03-31 18:29:50 +0300

    change the pattern, it now processes the date correctly, does not accept like '9999-99-99'

--- src/main/java/com/senla/weatheranalyzer/dto/AVGRequest.java
+++ src/main/java/com/senla/weatheranalyzer/dto/AVGRequest.java
@@ -3,9 +3,11 @@ package com.senla.weatheranalyzer.dto;
 import jakarta.validation.constraints.Pattern;
 
 public record AVGRequest(
-        @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "The date 'from' must be in the format yyyy-MM-dd")
+        @Pattern(regexp = "^(19|20)\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$",
+                message = "The date 'from' must be in the format yyyy-MM-dd with valid values for year, month, and day")
         String from,
-        @Pattern(regexp = "\\d{4}-\\d{2}-\\d{2}", message = "The date 'to' must be in the format yyyy-MM-dd")
+        @Pattern(regexp = "^(19|20)\\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$",
+                message = "The date 'to' must be in the format yyyy-MM-dd with valid values for year, month, and day")
         String to
 ) {
 }
